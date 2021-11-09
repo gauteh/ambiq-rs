6a7d8e59a6e6cfaa7353708d7d4ee0dd7c734b64
Gaute Hope <eg@gaute.vetsj.com>
2021-11-09 09:06:30 +0100

    boards: nano + more rtc alarm

--- ambiq-hal/src/rtc.rs
+++ ambiq-hal/src/rtc.rs
@@ -6,6 +6,14 @@ pub struct Rtc {
     rtc: RTC,
 }
 
+pub fn bcd_to_dec(bcd: u8) -> u8 {
+    (((bcd & 0xf0) >> 4) * 10) + (bcd & 0x0f)
+}
+
+pub fn dec_to_bcd(dec: u8) -> u8 {
+    ((dec / 10) << 4) | (dec % 10)
+}
+
 impl Rtc {
     pub fn new(rtc: RTC, clkgen: &mut CLKGEN) -> Rtc {
         // Enable XT for RTC
@@ -14,6 +22,8 @@ impl Rtc {
 
         // Select XT as source
         clk.rtc_use_xt();
+
+        rtc.rtcctl.modify(|_, w| w.hr1224()._24hr());
 
         Rtc { rtc }
     }
@@ -26,33 +36,76 @@ impl Rtc {
         self.rtc.rtcctl.modify(|_, w| w.rstop().stop());
     }
 
-    /// Get the current datetime (accurate to 100th second). Blocks untill no rollover
+    /// Get the current datetime (accurate to 1/100th second). Blocks untill no rollover
     /// error.
     pub fn now(&self) -> chrono::NaiveDateTime {
         let (upper, lower) = loop {
             let lower = self.rtc.ctrlow.read();
-            let upper = self.rtc.ctrup.read();
 
-            // Check whether rollover between read of lower or upper.
+            let no_err = self.rtc.ctrup.read().cterr().is_noerr(); // Set if upper read is done later than 1/100th sec after lower read.
+
+            let upper = self.rtc.ctrup.read(); // Resets error.
+
+            // Check for rollover between read of lower and upper.
             // p. 554.
-            if !self.rtc.ctrup.read().cterr().is_rderr() {
+            if no_err {
                 break (upper, lower);
             }
         };
 
         chrono::NaiveDate::from_ymd(
-            upper.ctryr().bits().into(),
-            upper.ctrmo().bits().into(),
-            upper.ctrdate().bits().into(),
+            bcd_to_dec(upper.ctryr().bits()).into(),
+            bcd_to_dec(upper.ctrmo().bits()).into(),
+            bcd_to_dec(upper.ctrdate().bits()).into(),
         )
         .and_hms_milli(
-            lower.ctrhr().bits().into(),
-            lower.ctrmin().bits().into(),
-            lower.ctrsec().bits().into(),
-            u32::from(lower.ctr100().bits()) * 10u32,
+            bcd_to_dec(lower.ctrhr().bits()).into(),
+            bcd_to_dec(lower.ctrmin().bits()).into(),
+            bcd_to_dec(lower.ctrsec().bits()).into(),
+            u32::from(bcd_to_dec(lower.ctr100().bits())) * 10u32,
         )
     }
+
+    /// Set the repeat alarm interval. Remember to enable the alarm as well.
+    pub fn set_alarm_repeat(&mut self, interval: AlarmRepeat) {
+        // TODO: Also support 1/10th and 1/100th second alarms.
+
+        unsafe {
+            // self.rtc.almup.write(|w| w.bits(0x0));
+            // self.rtc.almlow.write(|w| w.bits(0x0));
+            self.rtc.almup.reset();
+            self.rtc.almlow.reset();
+            // self.rtc.almlow.write(|w| w.alm100().bits(0));
+            // self.rtc.almlow.write(|w| w.alm100().bits(0));
+        }
+        self.rtc.rtcctl.modify(|_, w| w.rpt().variant(interval));
+    }
+
+    pub fn clear_interrupts(&mut self) {
+        unsafe {
+            // self.rtc.intclr.write(|w| w.bits(0x8));
+            self.rtc.intclr.write(|w| w.alm().set_bit());
+        }
+    }
+
+    pub fn disable_alarm_repeat(&mut self) {
+        self.rtc.rtcctl.modify(|_, w| w.rpt().dis());
+    }
+
+    pub fn enable_alarm(&mut self) {
+        self.clear_interrupts();
+        self.rtc.inten.write(|w| w.alm().set_bit());
+        unsafe {
+            pac::NVIC::unmask(pac::Interrupt::RTC);
+        }
+    }
+
+    pub fn disable_alarm(&mut self) {
+        self.rtc.inten.write(|w| w.alm().clear_bit());
+    }
 }
+
+pub type AlarmRepeat = pac::rtc::rtcctl::RPT_A;
 
 // impl Clock for Rtc {
 //     type T = u64;
--- boards/redboard-nano/examples/rtc_alarm.rs
+++ boards/redboard-nano/examples/rtc_alarm.rs
@@ -10,6 +10,8 @@ use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch
 
 use cortex_m::asm;
 use cortex_m_rt::entry;
+use hal::pac::interrupt;
+// use cortex_m_rt::interrupt;
 
 use ambiq_hal as hal;
 use hal::prelude::*;
@@ -29,21 +31,49 @@ fn main() -> ! {
     // set up serial
     let mut serial = hal::uart::Uart0::new(dp.UART0, pins.tx0, pins.rx0);
     uwriteln!(serial, "Hello world from RTC example!").unwrap();
+    uwriteln!(serial, "Setting up alarm every 1 second.").unwrap();
+    delay.delay_ms(500u32);
 
     // Set up RTC
     let mut rtc = hal::rtc::Rtc::new(dp.RTC, &mut dp.CLKGEN);
     rtc.enable();
 
-    let mut timestamp = 0;
+    rtc.set_alarm_repeat(hal::rtc::AlarmRepeat::SEC);
+    rtc.enable_alarm();
+
+    let mut timestamp = rtc.now().timestamp_millis();
     loop {
         led.toggle().unwrap();
-        delay.delay_ms(2000u32);
 
         let now = rtc.now().timestamp_millis();
 
-        uwriteln!(serial, "Loop iteration, timestamp millis: {}, difference = {} (should be about 2000)", now, (now - timestamp)).unwrap();
+        uwriteln!(serial, "Loop iteration, timestamp millis: {}, difference = {} (should be about 1000)", now, (now - timestamp)).unwrap();
 
         timestamp = now;
+
+        uwriteln!(serial, "Sleeping (WFI)..").unwrap();
+
+        cortex_m::asm::wfi();
+
+        // delay.delay_ms(100u32);
+        // cortex_m::asm::bkpt();
+
+        // rtc.disable_alarm();
+        // rtc.clear_interrupts();
     }
 }
 
+#[interrupt]
+fn CLKGEN() {
+    panic!();
+}
+
+#[interrupt]
+fn RTC() {
+    // panic!();
+    cortex_m::interrupt::free(|_| {
+        unsafe {
+            (*(hal::pac::RTC::ptr())).intclr.write(|w| w.alm().clear_bit());
+        }
+    });
+}
