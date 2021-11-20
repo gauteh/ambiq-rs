a3ac44d2e67cf8dba08f5d28221de2e3f69d8afa
Gaute Hope <eg@gaute.vetsj.com>
2021-11-20 23:11:05 +0100

    tools: run probe-run after flashing

--- tools/objcopy-bootload-svl.sh
+++ tools/objcopy-bootload-svl.sh
@@ -14,4 +14,7 @@ arm-none-eabi-objcopy -S -O binary "${TARGET}" "${BIN}"
 echo "flashing /dev/ttyUSB0.."
 python3 "${BOOTLOADER}" -f "${BIN}" /dev/ttyUSB0 -v
 
+echo "running probe-run.."
+probe-run --chip AMA3B1KK-KBR "${TARGET}" -v
+
 
