1f1a93e533013df9d8b15bbdd519e70989937325
Gaute Hope <eg@gaute.vetsj.com>
2021-12-09 12:57:59 +0100

    quickstart: some readme + svl default + rtc / uart as example

---
+++ quickstart/README.md
@@ -0,0 +1,30 @@
+# Quickstart with Rust on the Ambiq Apollo / Sparkfun Redboard Artemis
+
+This quickstart is adapted to the Sparkfun Redboard Artemis. The pins for e.g.
+LED or I2C ports differ between boards.
+
+## Installing the required toolchains and targets
+
+You essentially have to follow the [cortex-m-quickstart](https://github.com/rust-embedded/cortex-m-quickstart),
+start with adding the target for our MCU:
+
+```
+$ rustup target add thumbv7em-none-eabihf
+```
+
+## Build and upload using the Sparkfun bootloader
+
+```
+$ cargo run && tail /dev/ttyACM0
+```
+
+## Using probe-run
+
+If you have a debugger, and have [this patch](https://github.com/probe-rs/probe-rs/pull/855) applied to probe-rs (as used by
+probe-run), you can set the runner to be `probe-run` in `.cargo/config.toml`.
+
+## New projects
+
+Remember to include `build.rs`, `memory.x` and the `.cargo` subfolder in your
+project.
+
--- quickstart/src/main.rs
+++ quickstart/src/main.rs
@@ -13,23 +13,36 @@ use cortex_m_rt::entry;
 use ambiq_hal as hal;
 use hal::prelude::*;
 
-use rtt_target::{rtt_init_print, rprintln};
+use ufmt::uwriteln;
 
 #[entry]
 fn main() -> ! {
-    rtt_init_print!();
-    let mut peripherals = hal::pac::Peripherals::take().unwrap();
+    let mut dp = hal::pac::Peripherals::take().unwrap();
     let core = hal::pac::CorePeripherals::take().unwrap();
 
-    let mut delay = hal::delay::Delay::new(core.SYST, &mut peripherals.CLKGEN);
+    let mut delay = hal::delay::Delay::new(core.SYST, &mut dp.CLKGEN);
 
-    let pins = hal::gpio::Pins::new(peripherals.GPIO);
+    let pins = hal::gpio::Pins::new(dp.GPIO);
     let mut led = pins.d19.into_push_pull_output();
 
+    // set up serial
+    let mut serial = hal::uart::Uart0::new(dp.UART0, pins.tx0, pins.rx0);
+    uwriteln!(serial, "Hello world from RTC example!").unwrap();
+
+    // Set up RTC
+    let mut rtc = hal::rtc::Rtc::new(dp.RTC, &mut dp.CLKGEN);
+    rtc.enable();
+
+    let mut timestamp = rtc.now().timestamp_millis();
     loop {
         led.toggle().unwrap();
-        delay.delay_ms(1000u32);
-        rprintln!("Hello, world!");
+        delay.delay_ms(2000u32);
+
+        let now = rtc.now().timestamp_millis();
+
+        uwriteln!(serial, "Loop iteration, timestamp millis: {}, difference = {} (should be about 2000)", now, (now - timestamp)).unwrap();
+
+        timestamp = now;
     }
 }
 
